739f31dff891f580337c8bd231d83be1bc1d1c65
Atla33 <dantasanderson57@hotmail.com>
2024-09-09 21:31:22 -0300

    Atividade do mod 9

--- src/tarefa_wrappers/ConversaoWrapper.java
+++ src/tarefa_wrappers/ConversaoWrapper.java
@@ -5,21 +5,21 @@ import java.util.Scanner;
 public class ConversaoWrapper {
 
     public static void main(String[] args) {
-        // Cria um objeto Scanner para capturar a entrada do usuário
+
         Scanner scanner = new Scanner(System.in);
 
-        // Pede ao usuário para digitar um número
+
         System.out.println("Digite um número inteiro: ");
         int valorPrimitivo = scanner.nextInt();  // Variável primitiva
 
-        // Converte o tipo primitivo para o tipo wrapper
-        Integer valorWrapper = Integer.valueOf(valorPrimitivo);  // Tipo wrapper
 
-        // Imprime os valores
+        Integer valorWrapper = Integer.valueOf(valorPrimitivo);
+
+
         System.out.println("Valor primitivo: " + valorPrimitivo);
         System.out.println("Valor wrapper: " + valorWrapper);
 
-        // Fechar o scanner
+
         scanner.close();
     }
 }
